a3d404e144ed9a45367c0fbf2cca0b66512bdf12
Andrew de los Reyes <adlr@chromium.org>
2019-06-18 14:35:31 -0700

    WIP: Start of knob drawing support in DocView

---
+++ docview_unittest.cc
@@ -0,0 +1,22 @@
+// Copyright...
+
+#include <gtest/gtest.h>
+
+#include "docview.h"
+
+namespace formulate {
+
+TEST(DocViewTest, KnobsTest) {
+  SkRect obj = SkRect::MakeXYWH(10, 10, 20, 20);
+  EXPECT_EQ(SkRect::MakeXYWH(7, 17, 6, 6),
+            DocView::KnobRect(kMiddleLeftKnob, obj));
+  EXPECT_EQ(SkRect::MakeXYWH(6.5, 16.5, 7, 7),
+            DocView::KnobBounds(kMiddleLeftKnob, obj));
+  EXPECT_EQ(SkRect::MakeLTRB(6.5, 6.5, 33.5, 33.5),
+            DocView::KnobBounds(kAllKnobs, obj));
+  EXPECT_EQ(SkRect::MakeLTRB(6.5, 6.5, 33.5, 33.5),
+            DocView::KnobBounds(
+                kTopLeftKnob | kTopRightKnob | kBottomCenterKnob, obj));
+}
+
+}  // namespace formulate
