d42c87fe7fe52d1f9473d99776d811a4b3a70ba6
Andrew de los Reyes <adlr@chromium.org>
2019-06-19 17:12:26 -0700

    WIP: fix knob bounds unittest

--- docview_unittest.cc
+++ docview_unittest.cc
@@ -7,16 +7,44 @@
 namespace formulate {
 
 TEST(DocViewTest, KnobsTest) {
+  DocView doc;
   SkRect obj = SkRect::MakeXYWH(10, 10, 20, 20);
-  EXPECT_EQ(SkRect::MakeXYWH(7, 17, 6, 6),
-            DocView::KnobRect(kMiddleLeftKnob, obj));
-  EXPECT_EQ(SkRect::MakeXYWH(6.5, 16.5, 7, 7),
-            DocView::KnobBounds(kMiddleLeftKnob, obj));
-  EXPECT_EQ(SkRect::MakeLTRB(6.5, 6.5, 33.5, 33.5),
-            DocView::KnobBounds(kAllKnobs, obj));
-  EXPECT_EQ(SkRect::MakeLTRB(6.5, 6.5, 33.5, 33.5),
-            DocView::KnobBounds(
-                kTopLeftKnob | kTopRightKnob | kBottomCenterKnob, obj));
+  float knob_width = DocView::KnobWidth();
+  float border_width = DocView::KnobBorderWidth();
+  float line_width = 1;
+  SkRect outline_bounds = obj.makeOutset(line_width, line_width);
+  EXPECT_EQ(SkRect::MakeXYWH(10 - knob_width / 2,
+                             20 - knob_width / 2,
+                             knob_width,
+                             knob_width),
+            doc.KnobRect(kMiddleLeftKnob, obj));
+  {
+    SkRect temp_bounds = outline_bounds;
+    temp_bounds.join(SkRect::MakeXYWH(10 - knob_width / 2 - border_width / 2,
+                                      20 - knob_width / 2 - border_width / 2,
+                                      knob_width + border_width,
+                                      knob_width + border_width));
+    EXPECT_EQ(temp_bounds, doc.KnobBounds(kMiddleLeftKnob, obj));
+  }
+  {
+    SkRect temp_bounds = outline_bounds;
+    temp_bounds.join(SkRect::MakeLTRB(10 - knob_width / 2 - border_width / 2,
+                                      10 - knob_width / 2 - border_width / 2,
+                                      30 + knob_width / 2 + border_width / 2,
+                                      30 + knob_width / 2 + border_width / 2));
+    EXPECT_EQ(temp_bounds, doc.KnobBounds(kAllKnobs, obj));
+  }
+  {
+    SkRect temp_bounds = outline_bounds;
+    temp_bounds.join(SkRect::MakeLTRB(10 - knob_width / 2 - border_width / 2,
+                                      10 - knob_width / 2 - border_width / 2,
+                                      30 + knob_width / 2 + border_width / 2,
+                                      30 + knob_width / 2 + border_width / 2));
+    EXPECT_EQ(temp_bounds, doc.KnobBounds(kAllKnobs, obj));
+    EXPECT_EQ(temp_bounds,
+              doc.KnobBounds(kTopLeftKnob | kTopRightKnob | kBottomCenterKnob,
+                             obj));
+  }
 }
 
 TEST(DocViewTest, KnobTypeMaskTest) {
